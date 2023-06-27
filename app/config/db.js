c19b1c75c9d77b3b1271c6156eeda09608e6ded9
nutcras <83870987+nutcras@users.noreply.github.com>
2023-06-27 08:58:23 +0700

    update database reconnect

--- app/config/db.js
+++ app/config/db.js
@@ -1,26 +1,41 @@
-const { Pool } = require('pg'); // เรียกใช้งาน pg module
+const { Pool } = require("pg");
 
-// กำหนดการเชื่อมต่อฐานข้อมูล
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  // Enable automatic reconnection
+  idleTimeoutMillis: 1000, // Close idle clients after 1 second of inactivity
+  connectionTimeoutMillis: 5000, // Wait 5 seconds while trying to connect before timing out
 });
-// ทำการเชื่อมต่อกับฐานข้อมูล
-pool.connect((err, client, release) => {
-  if (err) {
-    // กรณีเกิด error
-    return console.error('Error acquiring client', err.stack)
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release();
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+});
+
+const connectWithRetry = () => {
+  console.log("Attempting to connect to PostgreSQL database...");
+  pool.connect((err, client, release) => {
     if (err) {
-      return console.error('Error executing query', err.stack)
+      console.error("Error acquiring client", err.stack);
+      setTimeout(connectWithRetry, 2000); // Retry after 2 seconds
+      return;
     }
-    console.log('Connected to PostgreSQL database at:', result.rows[0].now);
-  })
-})
+
+    client.query("SELECT NOW()", (err, result) => {
+      release();
+      if (err) {
+        console.error("Error executing query", err.stack);
+        setTimeout(connectWithRetry, 2000); // Retry after 2 seconds
+        return;
+      }
+      console.log("Connected to PostgreSQL database at:", result.rows[0].now);
+    });
+  });
+};
+
+connectWithRetry();
 
 module.exports = pool;
